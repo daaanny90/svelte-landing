1cab138ac70d32d14fe9b5b0dc7b7f87389d5314
daaanny90 <danny.spina@hotmail.it>
2022-05-31 12:00:06 +0200

    first commit

---
+++ src/routes/about.svelte
@@ -0,0 +1,4 @@
+<section class="text-center w-full h-screen bg-gradient-to-r from-blue-200 via-yellow-300 to-pink-300">
+<h1 class="pt-10 text-7xl">CONFGAMES IS COOL!</h1>
+<div class="font-serif pt-12 text-2xl">yeah!</div>
+</section>
